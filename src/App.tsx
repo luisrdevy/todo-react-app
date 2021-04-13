47a9fc1bb5af2e6bb22633bee807a850e3fbbec4
Luis Resendiz <luisrpw0@gmail.com>
2021-04-13 15:35:43 -0500

    Initial basic app without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    return (
+        <main>
+            <h1>Todos app</h1>
+            <h3>What needs to be done?</h3>
+            <form>
+                <input type="text"/>
+                <button>Add</button>
+            </form>
+            <section id="filters">
+                <button>Show all tasks</button>
+                <button>Show active tasks</button>
+                <button>Show completed tasks</button>
+            </section>
+            <section>
+                <h3>3 tasks remaining</h3>
+                <ul>
+                    <li>
+                        <div>
+                            <input id="todo-0" type="checkbox"/>
+                            <label htmlFor="todo-0">Learn useState</label>
+                            <div>
+                                <button>Edit</button>
+                                <button>Remove</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <input id="todo-1" type="checkbox"/>
+                            <label htmlFor="todo-1">Learn useEffect</label>
+                            <div>
+                                <button>Edit</button>
+                                <button>Remove</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <input id="todo-2" type="checkbox"/>
+                            <label htmlFor="todo-2">Learn useReducer</label>
+                            <div>
+                                <button>Edit</button>
+                                <button>Remove</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        </main>
+    )
 }
 
 export default App;
